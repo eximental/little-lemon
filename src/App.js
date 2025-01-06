a01998d381edc85fbbb38ca5d9c5cec8f23fffac
Kaspars T <kaspars.agent@gmail.com>
2025-01-06 07:21:21 +0200

    adding everything

--- src/App.js
+++ src/App.js
@@ -15,6 +15,14 @@ function App() {
       <Header />
       <main>
         <h1>Little Lemon</h1>
+        <div className="items-grid">
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+          <p>4</p>
+          <p>5</p>
+          <p>6</p>
+        </div>
       </main>
       <Footer />
     </>
